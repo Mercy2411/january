6595ad60989f50b5869e97f1963ab8aa41c9217a
Navis@12345* <mercyedvin24@gmail.com>
2021-12-14 17:40:44 +0530

    third

---
+++ src/org/base/Employee.java
@@ -0,0 +1,23 @@
+package org.base;
+
+public class Employee {
+public static void main(String[] args) {
+	byte age = 27;
+	System.out.println("Employee age is:"+ age);
+	short atmpin = 1234;
+	System.out.println("Employee atmpin is"+ atmpin);
+	int lanline = 12345678;
+	System.out.println("Employee lanline is"+ lanline);
+	long mobile = 123456789;
+	System.out.println("Employee mobile number"+mobile);
+	float salary = 75234.342f;
+		System.out.println("Employee salary is"+ salary);
+	float x = 40/3f;
+	System.out.println(x);
+	double y = 40/3d;
+	System.out.println(y);
+	char grade = 'A';
+	System.out.println("Employee grade is"+ grade);
+	
+	}
+}
